444accf381febc284c2d602d1cbb23f5ee973ca2
anshul202 <sainian45@gmail.com>
2024-01-15 18:27:09 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3jsCastle/",
+  base: "/3JSCASTLE/",
   assetsInclude: ['**/*.glb']
 })
